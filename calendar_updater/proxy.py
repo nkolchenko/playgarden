67ad082b0013de9c2a620e0602274fdbfb491651
nkolchenko <nkolchenko@gmail.com>
2016-02-12 18:15:34 +0100

    first working version

--- calendar_updater/proxy.py
+++ calendar_updater/proxy.py
@@ -1,2 +1,11 @@
 #!/usr/local/bin/python
 
+import shlex, subprocess
+
+task = "gcalcli --calendar test quick 'T1'"
+#task = "gcalcli agenda"
+
+args = shlex.split(str(task))
+
+print args
+send = subprocess.Popen(args) # I assume you already have Config file for this.
