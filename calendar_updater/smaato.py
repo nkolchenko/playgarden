3489d7b36d29c86841f795c70b1fe3a57eb2502d
nkolchenko <nkolchenko@gmail.com>
2016-02-16 11:50:15 +0100

    ver 0.02

--- calendar_updater/smaato.py
+++ calendar_updater/smaato.py
@@ -5,7 +5,7 @@ import sys
 import datetime
 from datetime import timedelta
 import MySQLdb
-
+import smtplib
 
 check_date = datetime.date.today() - timedelta(2)
 my_date = check_date.strftime('%Y%m%d')
@@ -16,17 +16,40 @@ cursor = connection.cursor ()
 
 my_query="select * from strikead where date="+my_date
 
+
+
 cursor.execute(my_query)
 data = cursor.fetchall ()
 
 for row in data :
-    print "Sizmek's daily statistic for Smaato\n"\
-          "date: "+str(row[0])+"\n"\
-          "bids: "+str(row[6])+"\n"\
-          "impressions: "+str(row[2])+"\n"\
-          "media_spent: "+str(row[3])+"\n"\
+    message_2 = "\n"\
+         "date: "+str(row[0])+"\n"\
+         "bids: "+str(row[6])+"\n"\
+         "impressions: "+str(row[2])+"\n"\
+         "media_spent: "+str(row[3])+"\n"
 
 cursor.close ()
 connection.close ()
+
+
+
+sender = 'from@fromdomain.com'
+receivers = ['nikolay@localhost']
+
+message_1 = """From: Nikolay <nikolay@localhost>
+To: nikolay@localhost
+
+Subject: Our daily statistic for Smaato for: """+str(check_date)+""".
+"""
+
+message = str(message_1)+str(message_2)
+print message
+
+try:
+   smtpObj = smtplib.SMTP('localhost')
+   smtpObj.sendmail(sender, receivers, message)
+   print "Successfully sent email"
+except SMTPException:
+   print "Error: unable to send email"
 
 sys.exit()
