f3dc338c0b39b77cc1f80aca1c7ac28ef6bf9f8d
nkolchenko <nikolay.kolchenko@strikad.com>
2016-05-06 16:45:34 +0200

    append insted overwrite

--- s3_bidlog_parser.sh
+++ s3_bidlog_parser.sh
@@ -12,7 +12,7 @@ for i in 0{0..9} {10..23}; do
 file="bids-"$date"_"$i"_00*"
 #echo $file 
 #aws s3 sync s3://strikead-data/logs/$DAY/ . --exclude '*' --include '*clicks*'
-aws s3 sync s3://strikead-data/logs/"$date"/ . --exclude '*' --include $file && awk -F"\t" '{if ($79==75997) print $1","$17","$18}' $file > 75997.csv && rm $file
+aws s3 sync s3://strikead-data/logs/"$date"/ . --exclude '*' --include $file && awk -F"\t" '{if ($79==75997) print $1","$17","$18}' $file >> 75997.csv && rm $file
 done
 
 
