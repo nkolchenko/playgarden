04d2850c1402d448a8a2df1daaf97f69a31e66dc
nkolchenko <nikolay.kolchenko@strikead.com>
2017-03-10 18:35:35 +0100

    v.01

--- python/statefarm.py
+++ python/statefarm.py
@@ -4,44 +4,38 @@ import urllib2
 import re
 import requests
 
-silex = 'silex_******' # obtain silex beforehand
+silex = '' # obtain silex beforehand
 
-collection = [
-#array of campaign ids
-]
+collection = [ 415019 ]
 
-manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
-handler = urllib2.HTTPBasicAuthHandler(manager)
-
-director = urllib2.OpenerDirector()
-director.add_handler(handler)
-
-# TODO: get rid of 'urllib' and replace it with 'requests'
 
 for creative_id in collection:
     try:
-        req = urllib2.Request('https://api.strikead.com/v1.1/creatives/'+str(creative_id), headers = {'X-Authorization' : silex })
+        payload = {'Content-Type': 'application/json','X-Authorization' : silex }
+        r = requests.get("https://api.strikead.com/v1.1/creatives/"+str(creative_id), headers=payload)
 
-        res = urllib2.urlopen(req)
-        body= res.read()
+#        print r.text
+#        print r.headers['content-type']
+#        print r.json()
 
-        creative_data = json.loads(body)
+        creative_data = r.json()
 
-        #print json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
-
+#        print json.dumps(creative_data, sort_keys=True, indent=4, separators=(',', ': '))
 
         if (creative_data["type"] == "image"):
             url = creative_data["tracking_code"]
 
-            substr = re.search('HREF=".*"',url)
+            substr = re.search('SRC=".* BORDER',url)
             substr = substr.group(0)
-
+#            print substr
             tr_code = substr.replace('\"', '')
-            tr_code = tr_code.replace('HREF=', '')
+            tr_code = tr_code.replace('SRC=', '')
+            tr_code = tr_code.replace(' BORDER', '')
+#            print tr_code
 
             creative_data["tracking_code"] = tr_code
 
-            #print json.dumps(creative_data, sort_keys=True, indent=4, separators=(',', ': '))
+#            print json.dumps(creative_data, sort_keys=True, indent=4, separators=(',', ': '))
 
             payload = {'Content-Type': 'application/json','X-Authorization' : silex }
             r = requests.put("https://api.strikead.com/v1.1/creatives/"+str(creative_id), json=creative_data, headers=payload)
