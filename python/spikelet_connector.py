12c24e0c040ab2d0a574e70d79805c5f4b4a6a0d
nkolchenko <nikolay.kolchenko@strikead.com>
2017-04-03 18:06:52 +0200

    module that allows fetching data from spikelet. v.1.01

--- python/spikelet_connector.py
+++ python/spikelet_connector.py
@@ -2,35 +2,37 @@
 
 """
 Module which contains functions to pull SSP's data from SpikeLet.
-
 """
 
 from __future__ import print_function
+
+import csv
+import json
 from itertools import islice
 
-import json
-import requests
-import csv
 import pymysql.cursors
+import requests
 
-#spikelet_host='spikelet.t5.va.us.strikead.com'
+# spikelet_host='spikelet.t5.va.us.strikead.com'
 
-spikelet_port=9000
-spikelet_url='http://{0}:{1}/report/v1/evaluate?token=staff'.format(spikelet_host,spikelet_port)
+spikelet_port = 9000
+spikelet_url = 'http://{0}:{1}/report/v1/evaluate?token=staff'.format(spikelet_host, spikelet_port)
 
-start_date='2017-03-29'
-end_date='2017-03-30'
+start_date = '2017-03-29'
+end_date = '2017-03-30'
 
 db_connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             db='discrepancy_check',
-                             charset='utf8',
-                             cursorclass=pymysql.cursors.DictCursor)
+                                user='root',
+                                password='',
+                                db='discrepancy_check',
+                                charset='utf8',
+                                cursorclass=pymysql.cursors.DictCursor)
+
 
 def main_cycle():
-    data=utc_fetch_from_spikelet(start_date, end_date)
+    data = utc_fetch_from_spikelet(start_date, end_date)
     store_in_mysql(data)
+
 
 def utc_fetch_from_spikelet(start_date, end_date):
     """
@@ -69,33 +71,33 @@ def utc_fetch_from_spikelet(start_date, end_date):
 
     # print(json_data)
     try:
-        r = requests.post(spikelet_url,json=json_data)  # XXX:
-        #print(r.text)
+        r = requests.post(spikelet_url, json=json_data)  # XXX:
+        # print(r.text)
     except requests.exceptions.ConnectionError:
-        print('Can\'t connect to http://{0}:{1} is it alive?'.format(spikelet_host,spikelet_port) )
+        print('Can\'t connect to http://{0}:{1} is it alive?'.format(spikelet_host, spikelet_port))
 
-
-
-    csv_data=r.content
-    #print(csv_data)
+    csv_data = r.content
+    # print(csv_data)
     return csv_data
+
 
 def store_in_mysql(csv_data):
     csv_lines = csv_data.splitlines()
     reader = csv.reader(csv_lines, delimiter=',')
 
-    with db_connection,db_connection.cursor() as cursor:
-        for row in islice(reader, 1, None):   # trick to get rid of leading row
-        #for row in reader:
-            date=row[1]
-            ssp=row[0]
-            impressions=row[2]
-            spent_imp=row[3]
+    with db_connection, db_connection.cursor() as cursor:
+        for row in islice(reader, 1, None):  # trick to get rid of leading row
+            # for row in reader:
+            date = row[1]
+            ssp = row[0]
+            impressions = row[2]
+            spent_imp = row[3]
             query = """INSERT INTO strikead (date, ssp, impressions, spent_imp) VALUES ("{0}", "{1}", {2}, {3});"""
-            sql = (query.format(date,ssp,impressions,spent_imp))
+            sql = (query.format(date, ssp, impressions, spent_imp))
             print(sql)
-            #cursor.execute(sql)
+            # cursor.execute(sql)
     db_connection.close()
+
 
 if __name__ == '__main__':
     main_cycle()
