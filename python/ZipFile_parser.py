11466545dd06253bbc66a19ef9399143286e7f55
nkolchenko <nikolay.kolchenko@strikead.com>
2017-05-08 14:48:15 +0200

    added  csv_parse

--- python/ZipFile_parser.py
+++ python/ZipFile_parser.py
@@ -3,71 +3,84 @@
 from __future__ import print_function
 import zipfile
 import os
+import csv
 from datetime import datetime, timedelta
 #import logging
 import traceback
 import sys
+import codecs
 
 
 ## logger = logging.getLogger(__name__)
 ## logger.setLevel(logging.INFO)
 ## logger = logging
+##logger.info("START")
 
 
 timestamp=datetime.today()
-today = datetime.date(timestamp - timedelta(11))
+today = datetime.date(timestamp - timedelta(1))
 
 date_for = datetime.strftime(today,'%Y_%m_%d')
 
 zip_file_dir='/Users/nkolchenko/playgarden/python/zip_files/'
 zip_file_name=zip_file_dir+'google_'+date_for+'.zip'
-
-##logger.info("START")
-
+dest=zip_file_dir+'google_'+date_for+'.csv'
 
 def fmt_last_exception():
     exc_info = traceback.format_exception(*sys.exc_info())
     return ''.join(exc_info)
 
+def extract_data_from_zip():
 
-try:
-    #raise ZeroDivisionError
-     with zipfile.ZipFile(zip_file_name, 'r') as myzip:
-         #print('is zip: '+str(zipfile.is_zipfile(zip_file_name)))
-         arch_file=zipfile.ZipFile.namelist(myzip)
-         #a={1,4,7,2,3}
-         #i = a.__len__()
-         i = arch_file.__len__()
+    try:
+        with zipfile.ZipFile(zip_file_name, 'r') as myzip:
+             arch_file=zipfile.ZipFile.namelist(myzip)
+             i = arch_file.__len__()
 
-         if i != 1:
-             print('got '+str(i)+' files instead of 1 inside archive: '+str(zip_file_name))
-         else:
-             ##print('file name in archive :'+arch_file[0])
-             ##print(zipfile.ZipFile.getinfo(myzip,arch_file[0]))
-             zipfile.ZipFile.extractall(myzip,zip_file_dir)
-             source=zip_file_dir+arch_file[0]
-             dest=zip_file_dir+'google_'+date_for+'.csv'
-             #print("src: "+source+"\ndst: "+dest)
-             os.rename(source,dest)
+             if i != 1:
+                 print('got '+str(i)+' files instead of 1 inside archive: '+str(zip_file_name))
+             else:
+                 ##print('file name in archive :'+file_list[0])
+                 ##print(zipfile.ZipFile.getinfo(myzip,file_list[0]))
+                 zipfile.ZipFile.extractall(myzip,zip_file_dir)
+                 source=zip_file_dir+arch_file[0]
+                 #dest=zip_file_dir+'google_'+date_for+'.csv'
+                 os.rename(source,dest)
 
-except IOError:
-    print("No such file or directory: "+str(zip_file_name))
+    except IOError:
+        print("No such file or directory: "+str(zip_file_name))
 
-
-
-except Exception as e:
-    print('foo', fmt_last_exception())
-    #print(type(e))
-    #print(dir(e))
+    except Exception as e:
+        print('foo', fmt_last_exception())
+        #print(type(e))
+        #print(dir(e))
 
 
-    # logger.exception('Ooops there is no: '+str(zip_file_name))
-    # raise
+def csv_parse():
 
+    try:
+
+        with codecs.open(dest, 'rb', 'utf-16') as csv_file:
+            reader = csv.DictReader(csv_file, delimiter='\t', quotechar='"')
+            for row in reader: #{'Advertiser cost': '5.238', 'Ad impressions (legacy)': '130,699', 'Days': '2017-04-05'}
+                return (row['Days'],
+                        'google',
+                        float(row['Advertiser cost']),
+                        int(row['Ad impressions (legacy)'].replace(',','')))
+
+
+    except Exception as e:
+        print('foo', fmt_last_exception())
+
+
+if __name__ == '__main__':
+    extract_data_from_zip()
+    csv_parse()
+
+
+# logger.exception('Ooops there is no: '+str(zip_file_name))
+# raise
 ###logger.info("DID")
-
 # PEP 8
-
 # (pip install ...) = six ; six.reraise (optional reading)
-
 # traceback, sys
